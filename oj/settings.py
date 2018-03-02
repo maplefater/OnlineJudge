fdaea62fb8929244eb8ec0070374c95b8c82dc27
MapleFater <602795977@qq.com>
2018-03-02 14:42:23 +0800

    add cors-header

--- oj/settings.py
+++ oj/settings.py
@@ -49,8 +49,8 @@ LOCAL_APPS = (
 INSTALLED_APPS = VENDOR_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
